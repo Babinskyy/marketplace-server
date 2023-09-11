32c1894a7be808791b1ab4dd31e750a72bf1841f
Babinskyy <janbbnsk@protonmail.com>
2023-09-11 17:26:24 +0200

    update

--- src/config/env-variable.ts
+++ src/config/env-variable.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 8000;
-export const NODE_ENV = process.env.MODE_ENV
+export const NODE_ENV = process.env.NODE_ENV
 export const MY_SETTING = process.env.MY_SETTING
 
 //db config
