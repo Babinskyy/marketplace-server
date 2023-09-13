421575087624b84848862012c58a5fa42dc07d28
Babinskyy <janbbnsk@protonmail.com>
2023-09-13 15:20:41 +0200

    update

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -3,35 +3,36 @@ import { Request, Response, NextFunction } from "express";
 
 const authMiddle = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies["AuthenticationToken"];
-  console.log(token);
-  if (token) {
-    try {
-      jwt.verify(
-        token,
-        "secretcode",
-        (
-          error: jwt.VerifyErrors | null,
-          decoded: string | jwt.JwtPayload | undefined
-        ) => {
-          if (error) {
-            return res
-              .status(401)
-              .json({ error: true, message: "Token is not valid" });
-          } else {
-            const userId = (decoded as jwt.JwtPayload).id;
-            res.locals.userId = userId;
-            next();
-          }
-        }
-      );
-    } catch {
-      console.log("no valid token catch");
-      res.status(401).json({ error: true, message: "not authorized" });
-    }
-  } else {
-    console.log("No token, authorization denied");
-    res.status(500).json({ error: true, message: "not authorized" });
-  }
+  console.log( "token: ", token);
+  // if (token) {
+  //   try {
+  //     jwt.verify(
+  //       token,
+  //       "secretcode",
+  //       (
+  //         error: jwt.VerifyErrors | null,
+  //         decoded: string | jwt.JwtPayload | undefined
+  //       ) => {
+  //         if (error) {
+  //           return res
+  //             .status(401)
+  //             .json({ error: true, message: "Token is not valid" });
+  //         } else {
+  //           const userId = (decoded as jwt.JwtPayload).id;
+  //           res.locals.userId = userId;
+  //           next();
+  //         }
+  //       }
+  //     );
+  //   } catch {
+  //     console.log("no valid token catch");
+  //     res.status(401).json({ error: true, message: "not authorized" });
+  //   }
+  // } else {
+  //   console.log("No token, authorization denied");
+  //   res.status(500).json({ error: true, message: "not authorized" });
+  // }
+  next();
 };
 
 export default authMiddle;
